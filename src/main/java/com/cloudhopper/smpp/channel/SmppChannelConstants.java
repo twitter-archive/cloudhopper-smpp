7a794903cd3d04c128bdcaea7c23a2c0c8bf1e85
Bojan Banko <bojan.banko@infobip.com>
2012-02-10 14:14:23 +0100

    forgot to commit the constants

--- src/main/java/com/cloudhopper/smpp/channel/SmppChannelConstants.java
+++ src/main/java/com/cloudhopper/smpp/channel/SmppChannelConstants.java
@@ -31,5 +31,6 @@ public class SmppChannelConstants {
     public static final String PIPELINE_SESSION_LOGGER_NAME = "smppSessionLogger";
     public static final String PIPELINE_SESSION_PDU_DECODER_NAME = "smppSessionPduDecoder";
     public static final String PIPELINE_SESSION_WRAPPER_NAME = "smppSessionWrapper";
+    public static final String PIPELINE_SESSION_SSL_NAME = "smppSessionSSL";
 
 }
