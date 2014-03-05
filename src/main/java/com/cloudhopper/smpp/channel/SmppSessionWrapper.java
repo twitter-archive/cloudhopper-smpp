259254aff24947b72054fa728bb43728a5f85aa6
Sergey S. Sergeev <webs.support@gmail.com>
2014-03-05 17:40:09 +0400

    * SmppSessionWrapper fixed (DefaultSmppServerTest)

--- src/main/java/com/cloudhopper/smpp/channel/SmppSessionWrapper.java
+++ src/main/java/com/cloudhopper/smpp/channel/SmppSessionWrapper.java
@@ -23,13 +23,9 @@ package com.cloudhopper.smpp.channel;
 import com.cloudhopper.smpp.impl.SmppSessionChannelListener;
 import com.cloudhopper.smpp.pdu.Pdu;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOutboundHandler;
-import io.netty.channel.ChannelPromise;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.net.SocketAddress;
 
 import static io.netty.channel.ChannelHandler.Sharable;
 
@@ -38,7 +34,7 @@ import static io.netty.channel.ChannelHandler.Sharable;
  * @author joelauer (twitter: @jjlauer or <a href="http://twitter.com/jjlauer" target=window>http://twitter.com/jjlauer</a>)
  */
 @Sharable
-public class SmppSessionWrapper extends SimpleChannelInboundHandler<Pdu> implements ChannelOutboundHandler {
+public class SmppSessionWrapper extends SimpleChannelInboundHandler<Pdu> {
     private static final Logger logger = LoggerFactory.getLogger(SmppSessionWrapper.class);
 
     private SmppSessionChannelListener listener;
@@ -47,57 +43,18 @@ public class SmppSessionWrapper extends SimpleChannelInboundHandler<Pdu> impleme
         this.listener = listener;
     }
 
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Pdu msg) throws Exception {
+        this.listener.firePduReceived(msg);
+    }
+
     /**
      * Invoked when a Channel was closed and all its related resources were released.
      */
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        logger.trace("channelInactive");
-        //TODO: if the old channelClosed is the outbound close(), we don't need this impl anymore
         this.listener.fireChannelClosed();
         super.channelInactive(ctx);
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#bind(SocketAddress, ChannelPromise)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception {
-	logger.trace("bind {}", localAddress);
-        ctx.bind(localAddress, promise);
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#connect(SocketAddress, SocketAddress, ChannelPromise)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception {
-	logger.trace("connect {} {}", localAddress, remoteAddress);
-        ctx.connect(remoteAddress, localAddress, promise);
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#disconnect(ChannelPromise)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
-	logger.trace("disconnect");
-        ctx.disconnect(promise);
-    }
-
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Pdu msg) throws Exception {
-        logger.trace("Pdu received: {}", msg);
-        this.listener.firePduReceived(msg);
     }
 
     /**
@@ -105,69 +62,6 @@ public class SmppSessionWrapper extends SimpleChannelInboundHandler<Pdu> impleme
      */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.trace("Exception triggered in upstream ChannelHandler: {}", cause.getCause());
         this.listener.fireExceptionThrown(cause.getCause());
     }
-
-    /**
-     * Calls {@link ChannelHandlerContext#close(ChannelPromise)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
-	logger.trace("close");
-	this.listener.fireChannelClosed();
-        ctx.close(promise);
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#close(ChannelPromise)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
-	logger.trace("deregister");
-        ctx.deregister(promise);
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#read()} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void read(ChannelHandlerContext ctx) throws Exception {
-	logger.trace("read");
-        ctx.read();
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#write(Object)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-	logger.trace("write {}", msg);
-        ctx.write(msg, promise);
-    }
-
-    /**
-     * Calls {@link ChannelHandlerContext#flush()} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void flush(ChannelHandlerContext ctx) throws Exception {
-	logger.trace("flush");
-        ctx.flush();
-    }
-
 }
