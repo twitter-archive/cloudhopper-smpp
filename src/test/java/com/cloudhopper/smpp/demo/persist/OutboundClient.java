d1a2cb2e84041402b598bac0de9b3d5c77069fee
Vojtech Krasa <vojta.krasa@gmail.com>
2014-04-03 14:34:07 +0200

    optimistic lock fix, copyright

--- src/test/java/com/cloudhopper/smpp/demo/persist/OutboundClient.java
+++ src/test/java/com/cloudhopper/smpp/demo/persist/OutboundClient.java
@@ -1,5 +1,26 @@
 package com.cloudhopper.smpp.demo.persist;
 
+/*
+ * #%L
+ * ch-smpp
+ * %%
+ * Copyright (C) 2009 - 2014 Cloudhopper by Twitter
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,12 +37,13 @@ import com.cloudhopper.smpp.type.SmppTimeoutException;
 
 public class OutboundClient extends Client {
 
+	private ReconnectionDaemon reconnectionDaemon = ReconnectionDaemon.getInstance();
+
 	private final ScheduledThreadPoolExecutor monitorExecutor;
 	private final ThreadPoolExecutor executor;
-	protected ScheduledExecutorService timer;
-	protected Integer enquireLinkPeriod = 1000;
-	protected Integer connectionFailedTimes = 0;
-	ReconnectionDaemon reconnectionDaemon = ReconnectionDaemon.getInstance();
+	private ScheduledExecutorService timer;
+	private Integer enquireLinkPeriod = 1000;
+	private volatile Integer connectionFailedTimes = 0;
 	private Logger logger = LoggerFactory.getLogger(OutboundClient.class);
 	private DefaultSmppClient clientBootstrap;
 	private DefaultSmppSessionHandler sessionHandler;
--- src/test/java/com/cloudhopper/smpp/demo/persist/ReconnectionTask.java
+++ src/test/java/com/cloudhopper/smpp/demo/persist/ReconnectionTask.java
@@ -1,5 +1,25 @@
 package com.cloudhopper.smpp.demo.persist;
 
+/*
+ * #%L
+ * ch-smpp
+ * %%
+ * Copyright (C) 2009 - 2014 Cloudhopper by Twitter
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,8 +41,10 @@ public class ReconnectionTask implements Runnable {
 		// sometimes it stain bound state even after SMSC is killed
 		if (!client.isConnected() && optimisticLock()) {
 			client.connect();
-		} else {
+		} else if (client.isConnected()) {
 			log.info("client {} is connected, skipping reconnect", client);
+		} else {
+			log.info("skipping duplicate reconnect for client {}", client);
 		}
 	}
 
